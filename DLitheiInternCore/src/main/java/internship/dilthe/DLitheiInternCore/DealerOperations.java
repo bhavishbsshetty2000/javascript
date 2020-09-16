44f1043271a9e2663f7d7606f643f855ce774f7e
bhavishbsshetty2000 <bhavishbsshetty2000@gmail.com>
2020-09-16 18:49:48 +0530

    HI

---
+++ DLitheiInternCore/src/main/java/internship/dilthe/DLitheiInternCore/DealerOperations.java
@@ -0,0 +1,14 @@
+package internship.dilthe.DLitheiInternCore;
+
+public interface DealerOperations 
+{
+	public void addToStock(Bike object);
+	public void listAll();
+	public void update(Integer cc,Integer newone);
+	public Bike[] shortlist(Short cc);
+	public Bike[] shortlist(Integer year);
+	public Bike[] shortlist(Double price);
+	public void sort();
+	public Bike remove(Integer index);
+}
+
